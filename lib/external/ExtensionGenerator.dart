7d709052950df5d1005c9eaa665888eed44f150c
TrueCollaboration <pshonka.dev@gmail.com>
2024-02-21 19:24:59 +0100

    pkg up

--- lib/external/ExtensionGenerator.dart
+++ lib/external/ExtensionGenerator.dart
@@ -7,7 +7,7 @@ import 'package:json_annotation_ex/library.dart';
 import 'package:json_serializable_ex/internal/ModelVisitor.dart';
 import 'package:recase/recase.dart';
 import 'package:source_gen/source_gen.dart';
-import 'package:foundation/library.dart';
+import 'package:pshondation/library.dart';
 
 class ExtensionGenerator extends GeneratorForAnnotation<JsonSerializableEx> {
   @override
